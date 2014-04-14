9344934371966a5fd53cc608ed2ab97d9a2c64ab
Mario Castro <mariocaster@gmail.com>
2014-04-14 22:04:33 +0200

    Pseudo distributed working version

--- prints-conf-files.sh
+++ prints-conf-files.sh
@@ -5,17 +5,17 @@ clear
 
 # core-site
 echo '############################ core-site.xml ############################'
-grep name $HADOOP_INSTALL/etc/hadoop/core-site.xml; grep value $HADOOP_INSTALL/etc/hadoop/core-site.xml
+grep name $HADOOP_HOME/etc/hadoop/core-site.xml; grep value $HADOOP_HOME/etc/hadoop/core-site.xml
 echo ''
 
 # hdfs-site
 echo '############################ hdfs-site.xml ############################################'
-grep name $HADOOP_INSTALL/etc/hadoop/hdfs-site.xml; grep value $HADOOP_INSTALL/etc/hadoop/hdfs-site.xml
+grep name $HADOOP_HOME/etc/hadoop/hdfs-site.xml; grep value $HADOOP_HOME/etc/hadoop/hdfs-site.xml
 echo ''
 
 # mapred-site
 echo '############################ mapred-site.xml ##########################################'
-grep name $HADOOP_INSTALL/etc/hadoop/mapred-site.xml; grep value $HADOOP_INSTALL/etc/hadoop/mapred-site.xml
+grep name $HADOOP_HOME/etc/hadoop/mapred-site.xml; grep value $HADOOP_HOME/etc/hadoop/mapred-site.xml
 echo ''
 
 # ~/.ssh/config
@@ -35,11 +35,11 @@ echo ''
 
 # masters
 echo '############################ masters ################################################'
-cat $HADOOP_INSTALL/etc/hadoop/masters
+cat $HADOOP_HOME/etc/hadoop/masters
 echo ''
 
 # slaves
 echo '############################ slaves #########################################33'
-cat $HADOOP_INSTALL/etc/hadoop/slaves
+cat $HADOOP_HOME/etc/hadoop/slaves
 echo ''
 
--- timer.sh
+++ timer.sh
@@ -15,6 +15,10 @@ else
 		-sy) start-yarn.sh
 			;;
 		-lsm) hdfs dfsadmin -safemode leave
+			;;
+		-wordcount) hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar wordcount /rime/pg4300.txt /rime/output
+			;;
+		-restart) stop-dfs.sh; stop-yarn.sh;start-dfs.sh;start-yarn.sh
 	esac
 fi
 
