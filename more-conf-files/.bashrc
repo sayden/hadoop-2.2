f741a9820a978d4d6a1e264cd1c9f8aaf7b54909
Mario Castro <mariocaster@gmail.com>
2014-04-15 09:16:24 +0200

    Minor changes in configuration

--- more-conf-files/.bashrc
+++ more-conf-files/.bashrc
@@ -9,15 +9,16 @@ fi
 # export SYSTEMD_PAGER=
 
 # Mario: User specific aliases and functions
-export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk
 #export HADOOP_INSTALL=/var/hadoop
-#export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_INSTALL/lib/native
-#export HADOOP_OPTS="-Djava.library.path=$HADOOP_INSTALL/lib"
-#export PATH=$PATH:$HADOOP_INSTALL/bin:$HADOOP_INSTALL/sbin
+
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk
 export HADOOP_HOME=/var/hadoop
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib"
 export HADOOP_MAPRED_HOME=$HADOOP_HOME
 export HADOOP_COMMON_HOME=$HADOOP_HOME
 export HADOOP_HDFS_HOME=$HADOOP_HOME
 export YARN_HOME=$HADOOP_HOME
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
--- prints-conf-files.sh
+++ prints-conf-files.sh
@@ -5,17 +5,22 @@ clear
 
 # core-site
 echo '############################ core-site.xml ############################'
-grep name $HADOOP_HOME/etc/hadoop/core-site.xml; grep value $HADOOP_HOME/etc/hadoop/core-site.xml
+grep "<name>" $HADOOP_HOME/etc/hadoop/core-site.xml; grep "<value>" $HADOOP_HOME/etc/hadoop/core-site.xml
 echo ''
 
 # hdfs-site
 echo '############################ hdfs-site.xml ############################################'
-grep name $HADOOP_HOME/etc/hadoop/hdfs-site.xml; grep value $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+grep "<name>" $HADOOP_HOME/etc/hadoop/hdfs-site.xml; grep "<value>" $HADOOP_HOME/etc/hadoop/hdfs-site.xml
 echo ''
 
 # mapred-site
 echo '############################ mapred-site.xml ##########################################'
-grep name $HADOOP_HOME/etc/hadoop/mapred-site.xml; grep value $HADOOP_HOME/etc/hadoop/mapred-site.xml
+grep "<name>" $HADOOP_HOME/etc/hadoop/mapred-site.xml; grep "<value>" $HADOOP_HOME/etc/hadoop/mapred-site.xml
+echo ''
+
+# yarn-site
+echo '########################### yarn-site.xml ############################################'
+grep "<name>" $HADOOP_HOME/etc/hadoop/yarn-site.xml; grep "<value>" $HADOOP_HOME/etc/hadoop/yarn-site.xml
 echo ''
 
 # ~/.ssh/config
@@ -33,13 +38,8 @@ echo '############################ /etc/hosts ##################################
 cat /etc/hosts
 echo ''
 
-# masters
-echo '############################ masters ################################################'
-cat $HADOOP_HOME/etc/hadoop/masters
+# masters y slaves
+echo '############################ masters / slaves ################################################'
+awk '{print "masters: " $1}' $HADOOP_HOME/etc/hadoop/masters
+awk '{print "slaves: " $1}' $HADOOP_HOME/etc/hadoop/slaves
 echo ''
-
-# slaves
-echo '############################ slaves #########################################33'
-cat $HADOOP_HOME/etc/hadoop/slaves
-echo ''
-
